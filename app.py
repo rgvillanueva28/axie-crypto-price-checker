122e5e0642ebe8b1a543ec4d0eba28d86f75223d
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2021-07-15 00:46:06 +0800

    Update to support all coins

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+all_tokens = requests.get("https://api.coingecko.com/api/v3/coins/list")
+all_tokens_json = all_tokens.json()
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'{client.user} has connected to Discord!')
@@ -15,25 +18,29 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user.id:
             return
-        
-        if message.content.lower().startswith("?p") or message.content.lower().startswith("?price"):
+            
+
+        if message.content.lower().startswith("/p") or message.content.lower().startswith("/price"):
             print(message.content)
             print(message.content.split())
-            if message.content.split()[1].lower() == "axs":
-                response = requests.get("https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xbb0e17ef65f82ab018d8edd776e8dd940327b28b&vs_currencies=php")
-                response_json = response.json()
-                price = response_json["0xbb0e17ef65f82ab018d8edd776e8dd940327b28b"]["php"]
-                print(price)
+            symbol = message.content.split()[1]
+            index = next((i for i, item in enumerate(all_tokens_json) if item["symbol"] == symbol), None)
 
-                await message.reply("{} Php per AXS\nSource: CoinGecko".format(price), mention_author=False)
-            
-            if message.content.split()[1].lower() == "slp":
-                response = requests.get("https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xcc8fa225d80b9c7d42f96e9570156c65d6caaa25&vs_currencies=php")
-                response_json = response.json()
-                price = response_json["0xcc8fa225d80b9c7d42f96e9570156c65d6caaa25"]["php"]
-                print(price)
+            if index != None:
+                id = all_tokens_json[index]["id"]
+                name = all_tokens_json[index]["name"]
                 
-                await message.reply("{} Php per SLP\nSource: CoinGecko".format(price), mention_author=False)
+                response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=php,usd".format(all_tokens_json[index]["id"]))
+                response_json = response.json()
+                price = "{:,.8f}".format(response_json[id]["usd"]).rstrip("0").rstrip(".") 
+                php = "{:,.8f}".format(response_json[id]["php"]).rstrip("0").rstrip(".") 
+
+                bot_message = "Name:\t{}\nSymbol:\t{}\nPrice:\t{} USD\nPhp:\t{} PHP".format(name, symbol.upper(), price, php)
+
+                await message.reply(bot_message, mention_author=False)
+
+            else:
+                await message.reply("Unknown coin. Baka na typo ka bobo.", mention_author=False)
 
 client = MyClient()
 client.run(TOKEN)
