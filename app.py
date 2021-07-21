2b462df726028062dd54648afb90078e51776942
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2021-07-21 23:06:38 +0800

    Added error handling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import requests
+from random import choice
 
 import discord
 from dotenv import load_dotenv
@@ -10,6 +11,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 all_tokens = requests.get("https://api.coingecko.com/api/v3/coins/list")
 all_tokens_json = all_tokens.json()
+
+unknown_coin = ["Unknown coin.", "Coin not found.", "Invalid coin."]
+unknown_coin_2 = ["Baka na typo ka bobo.", "Ayusin mo tanga.", "Gago ka ba? Just asking."]
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -21,24 +25,32 @@ class MyClient(discord.Client):
             
 
         if message.content.lower().startswith("?p") or message.content.lower().startswith("?price"):
-            symbol = message.content.lower().split()[1]
-            index = next((i for i, item in enumerate(all_tokens_json) if item["symbol"] == symbol), None)
+            if len(message.content.split()) == 2:
 
-            if index != None:
-                id = all_tokens_json[index]["id"]
-                name = all_tokens_json[index]["name"]
-                
-                response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=php,usd".format(all_tokens_json[index]["id"]))
-                response_json = response.json()
-                price = "{:,.8f}".format(response_json[id]["usd"]).rstrip("0").rstrip(".") 
-                php = "{:,.8f}".format(response_json[id]["php"]).rstrip("0").rstrip(".") 
+                symbol = message.content.lower().split()[1]
+                index = next((i for i, item in enumerate(all_tokens_json) if item["symbol"] == symbol), None)
 
-                bot_message = "Name:\t{}\nSymbol:\t{}\nPrice:\t{} USD\nPhp:\t{} PHP\nSource: Coingecko".format(name, symbol.upper(), price, php)
+                if index != None:
+                    id = all_tokens_json[index]["id"]
+                    name = all_tokens_json[index]["name"]
+                    
+                    response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=php,usd".format(all_tokens_json[index]["id"]))
+                    response_json = response.json()
+                    price = "{:,.8f}".format(response_json[id]["usd"]).rstrip("0").rstrip(".") 
+                    php = "{:,.8f}".format(response_json[id]["php"]).rstrip("0").rstrip(".") 
 
-                await message.reply(bot_message, mention_author=False)
+                    bot_message = "Name:\t{}\nSymbol:\t{}\nPrice:\t{} USD\nPhp:\t{} PHP\nSource:\tCoingecko".format(name, symbol.upper(), price, php)
 
+                    await message.reply(bot_message, mention_author=False)
+
+                else:
+                    await message.reply(f"{choice(unknown_coin)} {choice(unknown_coin_2)}", mention_author=False)
+
+            elif len(message.content.split()) == 1:
+                await message.reply("Lagyan mo ng coin bobo.")
+            
             else:
-                await message.reply("Unknown coin. Baka na typo ka bobo.", mention_author=False)
+                await message.reply("Too many parameters. Isa lang uyyy.")
 
 client = MyClient()
 client.run(TOKEN)
